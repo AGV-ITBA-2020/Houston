23a34e1bb04e146b746a1bc4c14068e88dc006d6
Javier Kipen <javier1kipen@gmail.com>
2020-12-25 19:34:32 -0300

    Adaptando la GUI a lo nuestro
    Se pusieron 3 páginas: Home, settings y data analysis. Se estructuró la forma de Home de manera de replicar la GUI v1. Faltaría agregar el matplotlib de la parte gráfica y luego unir las cosas con lo anterior.

--- Prev Tests/Modern GUI/ui_main.py
+++ Prev Tests/Modern GUI/ui_main.py
@@ -1321,7 +1321,7 @@ class Ui_MainWindow(object):
         self.label_top_info_2.setText(QCoreApplication.translate("MainWindow", u"| HOME", None))
         self.label_user_icon.setText(QCoreApplication.translate("MainWindow", u"WM", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"HOME", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Empty Page - By: Wanderson M. Pimenta", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Empty Page ", None))
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"Page Index 0", None))
         self.labelBoxBlenderInstalation.setText(QCoreApplication.translate("MainWindow", u"BLENDER INSTALLATION", None))
         self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Your Password", None))
